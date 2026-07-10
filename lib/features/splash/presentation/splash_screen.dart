437114e32c1eed31508afbe8a4e7c27d5a36288d
Uththara Nethmini <uththaranethmini2005@outlook.com>
2026-07-10 15:30:28 +0530

    feat: add splash animation controller

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -2,9 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:prepwise_ai/core/theme/app_colors.dart';
 import 'package:prepwise_ai/core/theme/app_radius.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+
+  @override
+  void initState()
+   {
+    super.initState();
+
+    _controller = AnimationController (vsync:this,
+    duration: const Duration(milliseconds: 1200),
+    );
+
+    _controller.forward();
+
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
