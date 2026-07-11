86825d571e0b3dd7da76f83da153dec269dd75cf
NethmiAdhithya <nethmiadhithya3@gmail.com>
2026-07-11 10:06:56 +0530

    feat: add navigation from splash screen to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
-// 1. Import your new login screen file here
-import 'features/login/presentation/login_screen.dart';
+// SplashScreen එක import කරගන්න
+import 'features/splash/presentation/splash_screen.dart';
 
 void main() {
   runApp(const PrepWiseAIApp());
@@ -16,8 +16,8 @@ class PrepWiseAIApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'PrepWise AI',
       theme: AppTheme.lightTheme,
-      // 2. Change this line from SplashScreen() to LoginScreen()
-      home: const LoginScreen(),
+      // Firstly show SplashScreen in app
+      home: const SplashScreen(),
     );
   }
 }
